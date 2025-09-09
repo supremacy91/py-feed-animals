3f025eaf0d8f78dfb32c1cb0f7c3371f0e4926aa
Maks Kaplunovskiy <kaplunovskymv@gmail.com>
2025-09-09 17:16:47 +0300

    implement py-feed-animals fix_1

--- app/main.py
+++ app/main.py
@@ -33,4 +33,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list["Animal"]) -> int:
-    return sum([animal.feed() for animal in animals])
+    return sum(animal.feed() for animal in animals)
